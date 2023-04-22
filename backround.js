df1c17a61b385b63610d4679117bedb284d223fe
Xotic <124195896+Xotic69@users.noreply.github.com>
2023-04-21 17:46:28 -0700

    Update backround.js

--- backround.js
+++ backround.js
@@ -19,3 +19,30 @@ chrome.action.onClicked.addListener(async (tab) => {
       text: nextState,
     });
 ...
+ ...
+    if (nextState === "ON") {
+      // Insert the CSS file when the user turns the extension on
+      await chrome.scripting.insertCSS({
+        files: ["focus-mode.css"],
+        target: { tabId: tab.id },
+      });
+    } else if (nextState === "OFF") {
+      // Remove the CSS file when the user turns the extension off
+      await chrome.scripting.removeCSS({
+        files: ["focus-mode.css"],
+        target: { tabId: tab.id },
+      });
+    }
+  }
+});
+{
+  ...
+  "commands": {
+    "_execute_action": {
+      "suggested_key": {
+        "default": "Ctrl+B",
+        "mac": "Command+B"
+      }
+    }
+  }
+}
